fe642e00a9c2412c9831c95b3e2d42075bd4b610
noahbds <eblenoah@gmail.com>
2024-03-07 18:11:31 +0100

    Updated User Interface For the description

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -7,29 +7,30 @@ net.Receive("StartMission", function()
 end)
 
 concommand.Add("open_mission_selector", function()
-    local frame = vgui.Create("DFrame")                          -- Create a new frame
-    frame:SetSize(300, 150)                                      -- Set the size of the frame
-    frame:Center()                                               -- Center the frame on the screen
-    frame:SetTitle("Select a Mission")                           -- Set the title of the frame
-    frame:MakePopup()                                            -- Make the frame appear
+    local frame = vgui.Create("DFrame")                                                    -- Create a new frame
+    frame:SetSize(400, 200)                                                                -- Set the size of the frame
+    frame:Center()                                                                         -- Center the frame on the screen
+    frame:SetTitle("Select a Mission")                                                     -- Set the title of the frame
+    frame:MakePopup()                                                                      -- Make the frame appear
 
-    local dropdown = vgui.Create("DComboBox", frame)             -- Create a new dropdown menu
-    dropdown:SetSize(280, 30)                                    -- Set the size of the dropdown menu
-    dropdown:SetPos(10, 50)                                      -- Set the position of the dropdown menu
-    dropdown:SetValue("Select a mission...")                     -- Set the default value of the dropdown menu
+    local dropdown = vgui.Create("DComboBox", frame)                                       -- Create a new dropdown menu
+    dropdown:SetSize(380, 30)                                                              -- Set the size of the dropdown menu
+    dropdown:SetPos(10, 50)                                                                -- Set the position of the dropdown menu
+    dropdown:SetValue("Select a mission...")                                               -- Set the default value of the dropdown menu
 
-    local files = file.Find("missions/*.txt", "DATA")            -- Find all mission files
-    for _, file in ipairs(files) do                              -- For each file...
-        local missionName = string.gsub(file, "_npcpos.txt", "") -- Get the mission name from the file name
-        dropdown:AddChoice(missionName)                          -- Add the mission to the dropdown menu
+    local files = file.Find("missions/*.txt", "DATA")                                      -- Find all mission files
+    for _, file in ipairs(files) do                                                        -- For each file...
+        local missionName = string.gsub(file, "_npcpos.txt", "")                           -- Get the mission name from the file name
+        local missionDescription = file.Read("missions/" .. file, "DATA"):match("^(.-)\n") -- Read the mission description from the file
+        dropdown:AddChoice(missionName .. " - " .. missionDescription)                     -- Add the mission to the dropdown menu with the description
     end
 
-    local startButton = vgui.Create("DButton", frame)       -- Create a new button
-    startButton:SetSize(280, 30)                            -- Set the size of the button
-    startButton:SetPos(10, 90)                              -- Set the position of the button
-    startButton:SetText("Start Mission")                    -- Set the text of the button
-    startButton.DoClick = function()                        -- Set what happens when the button is clicked
-        local selectedMission = dropdown:GetValue()         -- Get the selected mission
-        RunConsoleCommand("start_mission", selectedMission) -- Start the selected mission
+    local startButton = vgui.Create("DButton", frame)                -- Create a new button
+    startButton:SetSize(380, 30)                                     -- Set the size of the button
+    startButton:SetPos(10, 130)                                      -- Set the position of the button
+    startButton:SetText("Start Mission")                             -- Set the text of the button
+    startButton.DoClick = function()                                 -- Set what happens when the button is clicked
+        local selectedMission = dropdown:GetValue():match("^(.-) -") -- Get the selected mission name from the dropdown value
+        RunConsoleCommand("start_mission", selectedMission)          -- Start the selected mission
     end
 end)

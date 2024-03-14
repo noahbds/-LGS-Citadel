f46ec8c9649df4cb68acfb946a9fc2f902d22220
noahbds <96846966+noahbds@users.noreply.github.com>
2024-03-14 02:53:54 +0100

    Update cl_init.lua

--- lua/autorun/client/cl_init.lua
+++ lua/autorun/client/cl_init.lua
@@ -19,28 +19,28 @@ net.Receive("StartMission", function()
 end)
 
 concommand.Add("open_mission_selector", function()
-    local frame = vgui.Create("DFrame")                                                        -- Create a new frame
-    frame:SetSize(500, 300)                                                                    -- Set the size of the frame
-    frame:Center()                                                                             -- Center the frame on the screen
-    frame:SetTitle("Select a Mission")                                                         -- Set the title of the frame
-    frame:MakePopup()                                                                          -- Make the frame appear
+    local frame = vgui.Create("DFrame")                                                    -- Create a new frame
+    frame:SetSize(500, 300)                                                                -- Set the size of the frame
+    frame:Center()                                                                         -- Center the frame on the screen
+    frame:SetTitle("Select a Mission")                                                     -- Set the title of the frame
+    frame:MakePopup()                                                                      -- Make the frame appear
 
-    local listview = vgui.Create("DListView", frame)                                           -- Create a new list view
-    listview:SetSize(480, 150)                                                                 -- Set the size of the list view
-    listview:SetPos(10, 50)                                                                    -- Set the position of the list view
-    listview:AddColumn("Mission")                                                              -- Add a column to the list view
+    local listview = vgui.Create("DListView", frame)                                       -- Create a new list view
+    listview:SetSize(480, 150)                                                             -- Set the size of the list view
+    listview:SetPos(10, 50)                                                                -- Set the position of the list view
+    listview:AddColumn("Mission")                                                          -- Add a column to the list view
 
-    local descriptionLabel = vgui.Create("DLabel", frame)                                      -- Create a new label
-    descriptionLabel:SetSize(480, 50)                                                          -- Set the size of the label
-    descriptionLabel:SetPos(10, 210)                                                           -- Set the position of the label
+    local descriptionLabel = vgui.Create("DLabel", frame)                                  -- Create a new label
+    descriptionLabel:SetSize(480, 50)                                                      -- Set the size of the label
+    descriptionLabel:SetPos(10, 210)                                                       -- Set the position of the label
 
-    local files = file.Find("missions/*.txt", "DATA")                                          -- Find all mission files
-    for _, file in ipairs(files) do                                                            -- For each file...
-        local missionName = string.gsub(file, "_npcpos.txt", "")                               -- Get the mission name from the file name
-        local missionFile = file.Read("missions/" .. selectedMission .. "_npcpos.txt", "DATA") -- Read the mission file                 -- Read the mission file
+    local files = file.Find("missions/*.txt", "DATA")                                      -- Find all mission files
+    for _, file in ipairs(files) do                                                        -- For each file...
+        local missionName = string.gsub(file, "_npcpos.txt", "")                           -- Get the mission name from the file name
+        local missionFile = file.Read("missions/" .. missionName .. "_npcpos.txt", "DATA") -- Read the mission file                 -- Read the mission file
         local missionDescription = missionFile and missionFile:match("^(.-)\n") or
-            ""                                                                                 -- Get the mission description from the file
-        listview:AddLine(missionName)                                                          -- Add the mission to the list view
+            ""                                                                             -- Get the mission description from the file
+        listview:AddLine(missionName)                                                      -- Add the mission to the list view
     end
 
     local startButton = vgui.Create("DButton", frame) -- Create a new button
